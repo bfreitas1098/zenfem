be27388575034fde092486f0ce2a6071d28fce72
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2023-10-19 21:20:41 -0400

    Added the design style guide to the index.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>ZenFem</h1>
+      <h1>Zenfem</h1>
+      <h3>This is a test</h3>
+      <p>This is a demonstration</p>
     </div>
   );
 }
