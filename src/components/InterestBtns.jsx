28b9c78f9e5ad36dba862c50ea70a2aa83bd5d01
Ellen <ellen.fitzgerald26@gmail.com>
2023-10-23 10:35:48 +0100

    backend

--- src/components/InterestBtns.jsx
+++ src/components/InterestBtns.jsx
@@ -1,4 +1,21 @@
+import React from "react";
 import "./InterestBtns.css";
+
+// const InterestBtns = (props) => {
+//   return (
+//     <div className="interests-btns">
+//       {props.interests ? (
+//         JSON.parse(props.interests.replace(/'/g, '"')).map((interest, index) => (
+//           <button key={index}>{interest}</button>
+//         ))
+//       ) : (
+//         <p>No interests provided.</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default InterestBtns;
 
 const InterestBtns = (props) => {
   return (
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,48 +2,8 @@ import "./UserCard.css";
 import add from "../images/add.svg";
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-
-
-// const UserCard = (props) => {
-//   return (
-//     <div className="user-card">
-//       <div className="bio-container">
-//         <img
-//           src={props.img}
-//           alt="A woman with earphones on that are connected to her phone that she is holding and looking happily in the distance in an autumnal setting"
-//         />
-//         <div className="name-connect-container">
-//           <div className="name">
-//             <p className="full-name">{props.name}</p>
-//             <p className="location">Pasadena, California</p>
-//           </div>
-//           <button className="btn--connect">
-//             <img src={add} alt="Plus icon" />
-//             Connect
-//           </button>
-//         </div>
-//       </div>
-//       <div className="interests-container">
-//         <h6>Interests</h6>
-//         <div className="interests-btns">
-//           <button>Sports</button>
-//           <button>Nature</button>
-//           <button>Knitting</button>
-//         </div>
-//       </div>
-//       <div className="goal-container">
-//         <h6>Current Goal</h6>
-//         <p className="goal">Swimming on Long Island Beach</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UserCard;
-
-
-
 import InterestBtns from "./InterestBtns";
+
 
 const UserCard = (props) => {
   const [userData, setUserData] = useState({});
@@ -92,18 +52,12 @@ const UserCard = (props) => {
           <div className="interests-container">
             <h6>Interests</h6>
             <div className="interests-btns">
-              {userData.interests ? (
-                JSON.parse(userData.interests.replace(/'/g, '"')).map((interest, index) => (
-                  <button key={index}>{interest}</button>
-                ))
-              ) : (
-                <p>No interests provided.</p>
-              )}
+            <InterestBtns interests={userData.interests} />
             </div>
           </div>
           <div className="goal-container">
             <h6>Current Goal</h6>
-            <p className="goal"></p>
+            <p className="goal">Swimming on Long Island Beach</p>
           </div>
         </div>
       )}
